2b9b3f49a7eae8f9990a97fe22639e7e794af9b8
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2022-11-20 17:27:20 +0200

    First try at dark mode switch

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,48 @@
-import '../styles/global.css'
+import { createContext, useEffect, useMemo, useState } from "react";
 import { SessionProvider } from "next-auth/react"
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline, useMediaQuery } from "@mui/material"
+import { createTheme } from "@mui/material/styles";
 
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 export default function App({ 
   Component, 
   pageProps: { session, ...pageProps } 
 }) {
+  const [mode, setMode] = useState('light')
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  useEffect(() => {
+    setMode(prefersDarkMode  ? 'dark': 'light')
+  }, [prefersDarkMode])
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+      }
+    }),
+    [],
+  )
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        }
+      }),
+    [mode],
+  )
+
   return(
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <SessionProvider session={session}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </ColorModeContext.Provider>
   )
 }
