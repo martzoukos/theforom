c692db4340e8cc033c182f6d12c7211b3357c257
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2022-11-21 23:00:26 +0200

    Improve the uimode setting logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import '../styles/variables/animations.css'
+import '../styles/variables/colors.css'
+import '../styles/variables/typography.css'
+import '../styles/variables/layout.css'
 import '../styles/global.css'
+
 import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react"
 import { useUIModeStore } from '../lib/UIModeStore'
+import DetectDarkMode from 'detect-dark-mode'
 
 export default function App({ 
   Component, 
@@ -11,11 +17,21 @@ export default function App({
   const storeModeToggle = useUIModeStore(state => state.toggle)
 
   useEffect(() => {
-    const localMode = (localStorage.getItem("UIMode") !== null)
-      ? localStorage.getItem('UIMode')
-      : null
-    storeModeToggle(localMode)
-    document.body.className = localMode ? localMode : storeMode
+    // 1. Application's session (default: 'light')
+    let targetValue = storeMode
+
+    // 2. Override with User's system preference, if is 'black'
+    if (DetectDarkMode.isDark) {
+      targetValue = 'dark'
+    }
+
+    // 3. Override with User's local storage (if available)
+    if (localStorage.getItem("UIMode") !== null) {
+      targetValue = localStorage.getItem('UIMode')
+    }
+    
+    storeModeToggle(targetValue)
+    document.body.className = targetValue
   }, [storeMode, storeModeToggle])
 
   return(
