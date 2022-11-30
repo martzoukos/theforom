a112869481bffa2d024c223b6c817c16d54fd834
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2022-11-30 15:18:52 +0200

    Redirect to home if signed in

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,10 +1,16 @@
-import { getCsrfToken } from "next-auth/react"
+import { getCsrfToken, useSession } from "next-auth/react"
 import Layout, { siteTitle } from '../components/Layout';
 import Head from "next/head";
 import Container from "../components/Container";
 import SignInForm from "../components/SignInForm";
+import { useRouter } from "next/router";
 
 export default function SignIn({ csrfToken }) {
+  const { data: session } = useSession()
+  const router = useRouter()
+  if (session) {
+    router.push('/')
+  }
   return (
     <Layout>
       <Head>
